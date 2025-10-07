972e2dbaa389049ad7ac078343026a73c09f7bf6
Ilham Riadi <ilhamriadi97@gmail.com>
2025-10-07 12:26:50 +0200

    first commit

---
+++ scripts/svg-fix.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "🧼 Membersihkan semua file SVG..."
+
+find public/images -type f -name "*.svg" | while read -r file; do
+  sed -i '1s/^[^<]*//' "$file"
+  iconv -f UTF-8 -t UTF-8 "$file" -o "$file.tmp" && mv "$file.tmp" "$file"
+done
+
+echo "✅ Semua file SVG sudah dibersihkan!"
